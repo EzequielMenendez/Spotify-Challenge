d3cbf705a8bb574f9a45999a2949713c15d8aa27
EzequielMenendez <ezemenendez2003@gmail.com>
2023-11-25 18:45:34 -0300

    add conection with db

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,24 @@
 import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import connectDb from './db'
+
+dotenv.config()
+
+if(process.env.DB_URL){
+    connectDb(process.env.DB_URL)
+}
 
 const app = express()
+app.use(cors({
+    origin: '*',
+    credentials: true
+}))
 app.use(express.json())
+app.use(morgan('dev'))
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, ()=>{
     console.log(`Server running in port ${PORT}`)
