4cd7105f0c6669d4f484c4244fe96068b0424a3a
EzequielMenendez <ezemenendez2003@gmail.com>
2023-11-26 13:35:29 -0300

    add responsive desing

--- client/src/components/Menu.tsx
+++ client/src/components/Menu.tsx
@@ -2,15 +2,15 @@ import michaeljackson from '../utils/michaeljackson.jpg'
 
 const Menu = () => {
     return(
-        <div className='border border-black rounded-2xl'>
-            <div className='flex items-center border border-black rounded-md place-content-around m-10'>
-                <img src={michaeljackson} alt="Michael Jackson" className='rounded-full w-32'/>
-                <h2 className='text-xl'>Michael Jackson</h2>
+        <div className='flex flex-col items-center justify-center border border-black rounded-2xl gap-10 w-52 sm:w-96 xl:w-[36rem]'>
+            <div className='flex items-center border border-black rounded-md place-content-around w-10/12 pt-1 pb-1 mt-10'>
+                <img src={michaeljackson} alt="Michael Jackson" className='rounded-full w-16 sm:w-24'/>
+                <h2 className='text-lg ml-1'>Michael Jackson</h2>
             </div>
-            <div className='flex flex-col items-center border border-black rounded-2xl m-10'>
-                <p>Song 1</p>
-                <p>Song 2</p>
-                <p>Song 3</p>
+            <div className='flex flex-col items-center border border-black rounded-2xl w-10/12 h-3/6 mb-10'>
+                <p className='border border-y-black w-full p-3'>Song 1</p>
+                <p className='border border-y-black w-full p-3'>Song 2</p>
+                <p className='border border-y-black w-full p-3'>Song 3</p>
             </div>
         </div>
     )
--- client/src/views/Main.tsx
+++ client/src/views/Main.tsx
@@ -1,10 +1,12 @@
 import Menu from "../components/Menu"
+import album from "../utils/Michael_Jackson_Album.png"
 
 const Main = ()=> {
     return(
         <div>
-            <div>
+            <div className="flex items-center justify-center gap-10 flex-col md:flex-row">
                 <Menu />
+                <img src={album} alt="album Thriller" className="rounded-2xl w-48 h-auto sm:w-80 xl:w-96"/>
             </div>
         </div>
     )
